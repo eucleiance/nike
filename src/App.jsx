624c39bd8d7dce611f0099c1d800f88652fd2c07
eucleiance <eucleiance@gmail.com>
2024-07-08 09:30:21 +0545

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-export default function App() {
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+      <section className="padding"> PopularProducts</section>
+      <section className="padding"> SuperQualty</section>
+      <section className="padding-x py-10"> Services</section>
+      <section className="padding"> SpecialOffer</section>
+      <section className="padding bg-blue-50"> CustomerReviews</section>
+      <section className="padding padding-x sm:py-32 py-16 w-full"> Subscribe</section>
+      <section className="padding bg-black padding-x padding-t pb-8"> Footer</section>
+    </main>
+  );
+};
+
+export default App;
