f078c8ef365d195477fd63ab3b24a8e0d2668dcb
eucleiance <eucleiance@gmail.com>
2024-07-09 15:27:58 +0545

    base 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+import { Hero, PopularProducts, SuperQuality, Services, SpecialOffer, CustomerReviews, Subscribe, Footer } from './sections'
+
 const App = () => {
   return (
     <main className="relative">
       Nav
-      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
-      <section className="padding"> PopularProducts</section>
-      <section className="padding"> SuperQuality</section>
-      <section className="padding-x py-10"> Services</section>
-      <section className="padding"> SpecialOffer</section>
-      <section className="padding bg-blue-50"> CustomerReviews</section>
-      <section className="padding padding-x sm:py-32 py-16 w-full"> Subscribe</section>
-      <section className="padding bg-black padding-x padding-t pb-8"> Footer</section>
+      <section className="xl:padding-1 wide:padding-r padding-b"><Hero/></section>
+      <section className="padding"> <PopularProducts/></section>
+      <section className="padding"> <SuperQuality/></section>
+      <section className="padding-x py-10"> <Services/></section>
+      <section className="padding"> <SpecialOffer/></section>
+      <section className="padding bg-blue-50"> <CustomerReviews/></section>
+      <section className="padding padding-x sm:py-32 py-16 w-full"> <Subscribe/></section>
+      <section className="padding bg-black padding-x padding-t pb-8"> <Footer/></section>
     </main>
   );
 };
